c620b4da4a763647e59eca5f65b124bd8499cbee
rohanphanse <rohan.a.phanse@gmail.com>
2025-01-07 12:26:40 -0800

    Github pages

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 const PORT = 3000
-app.use(express.static("public"))
+
+app.use((req, res, next) => {
+    console.log(req.path)
+    if (req.path.startsWith("/node_modules") || req.path == "/package.json" || req.path == "/package-lock.json") {
+        return res.status(403).send('Forbidden');
+    }
+    next();
+})
+app.use(express.static(path.join(__dirname, "/")))
+
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
 })
